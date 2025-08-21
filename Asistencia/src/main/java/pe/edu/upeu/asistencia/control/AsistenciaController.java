7600dc1a9420e80f71eb8fb989265ab9858cf4f3
Your Name <josepjesus93@gmail.com>
2025-08-21 11:30:55 -0500

    sumas

--- Asistencia/src/main/java/pe/edu/upeu/asistencia/control/AsistenciaController.java
+++ Asistencia/src/main/java/pe/edu/upeu/asistencia/control/AsistenciaController.java
@@ -1,7 +1,25 @@
 package pe.edu.upeu.asistencia.control;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class AsistenciaController {
-}
+
+    @FXML
+    TextField txtNum1, txtNum2;
+    @FXML Label txtResult;
+
+    @FXML
+    public void sumar() {
+        double num1 = Double.parseDouble(txtNum1.getText());
+        double num2 = Double.parseDouble(txtNum2.getText());
+        double resultado = num1 + num2;
+        txtResult.setText(String.valueOf(resultado));
+        }
+    }
+
