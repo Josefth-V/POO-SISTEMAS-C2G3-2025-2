c619b621e396b02bf4bc155fb4152ced441e937b
SebastianSumi <cspam7780@gmail.com>
2025-08-28 12:08:38 -0500

    trab03

---
+++ poo-sisc2g3/Asistencia/src/main/java/pe/edu/upeu/asistencia/Aplication.java
@@ -0,0 +1,8 @@
+package pe.edu.upeu.asistencia;
+
+public class Aplication {
+    public static void main(String[] args) {
+        System.out.println("Iniciando asistencia JOSE");
+        AsistenciaApplication.main(args);
+    }
+}
